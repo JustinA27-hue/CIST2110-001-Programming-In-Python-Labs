d709d58fb184eaeff6ea2dc3b94ad67d72b036eb
“JustinA27-huegit config --global user.email “agostoj2@go.stockton.edu <agostoj2@go.stockton.edu>
2023-09-26 19:22:31 -0400

    finish lab 1

--- Lesson1/Lab1.py
+++ Lesson1/Lab1.py
@@ -1,5 +1,5 @@
-# Lab1
-# Author: Greg Walsh
+# Lab1 
+# Author: Justin 
 
 
 # 1. Uncomment the following print statement and run the program. Show the output to your instructor.
@@ -10,8 +10,8 @@ print("Hello World!")
 
 # 2. Add a print statement that prints your name. Run the program and show the output to your instructor.
 
-print("Greg Walsh")
+print ("Justin Agosto")
 
 # 3. Add a print statement that prints your favorite color. Run the program and show the output to your instructor.
 
-print("Black")
+print ("purple")
