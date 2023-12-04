ec00bca5a43a0d280c090eb5368c4757b8a65810
“JustinA27-huegit config --global user.email “agostoj2@go.stockton.edu <agostoj2@go.stockton.edu>
2023-12-04 01:10:27 -0500

    completed lab 9 and 10

--- Week12/Lab10.py
+++ Week12/Lab10.py
@@ -1,5 +1,5 @@
 # Lab 10
-# Author:
+# Author: Justin Agosto
 
 # Lab 10 will show demonstrate how Dictionary's work in Python.
 
@@ -11,24 +11,32 @@
 
 
 # 2. Print out the dictionary.
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "Trenton",
+    "state": "New Jersey"
+}
 
+print(my_dict)
 
 # 3. Print out the value for the key "name".
-
+name = my_dict["name"]
+print(name)
 # 4. Lookup the key associated with the value "New Jersey" and print it out.
 # Hint 1: You will need to loop through the dictionary.
 # Hint 2: remember you can use the .items() method to get the key and value.
 
 # 5. Add a new key value pair to the dictionary.
 # Key: "country", Value: "USA"
-
+my_dict["country"] = "USA"
 
 # 6. Print out the dictionary.
-
+print(my_dict)
 # 7. Remove the key value pair with the key "city".
-
+removed_city = my_dict.pop("city")
 # 8. Print out the dictionary.
-
+print(my_dict)
 # 9. Create a dictionary called cities with an key as the City name and values as a dictionary that contains the state, population, and country.
 # use the following data:
 # Trenton, New Jersey, 84,913, USA
@@ -40,17 +48,67 @@
 # Philadelphia, Pennsylvania, 1,584,138, USA
 # San Antonio, Texas, 1,547,253, USA
 # San Diego, California, 1,423,851, USA
-
+cities = {
+    "Trenton": {
+        "state": "New Jersey",
+        "population": 84913,
+        "country": "USA"
+    },
+    "New York City": {
+        "state": "New York",
+        "population": 8336817,
+        "country": "USA"
+    },
+    "Los Angeles": {
+        "state": "California",
+        "population": 3979576,
+        "country": "USA"
+    },
+    "Chicago": {
+        "state": "Illinois",
+        "population": 2693976,
+        "country": "USA"
+    },
+    "Houston": {
+        "state": "Texas",
+        "population": 2320268,
+        "country": "USA"
+    },
+    "Phoenix": {
+        "state": "Arizona",
+        "population": 1680992,
+        "country": "USA"
+    },
+    "Philadelphia": {
+        "state": "Pennsylvania",
+        "population": 1584138,
+        "country": "USA"
+    },
+    "San Antonio": {
+        "state": "Texas",
+        "population": 1547253,
+        "country": "USA"
+    },
+    "San Diego": {
+        "state": "California",
+        "population": 1423851,
+        "country": "USA"
+    }
+}
 # 10. Print a table of the data using the pandas library.
 # pip install pandas
 
+import pandas as pd
+df = pd.DataFrame(cities)
+print(df)
 
 # 11. Use the tabulate library to print out the table.
 # pip install tabulate
-
+import tabulate
+print(tabulate.tabulate(cities, headers="keys"))
 
 # 12. Print out the population for the city of Chicago.
-
+print(cities["Chicago"]["population"])
 
 # 13. **Extra** An alternative to converting a dictionary into a pandas df then tabulate, you can use dictionary unpacking and tabulate.
 # Dictionary unpacking is a new feature in Python 3.9 and allows you to unpack a dictionary into a list of dictionaries.
@@ -67,6 +125,13 @@
 
 
 # 14. How many cities have a population greater than 1 million? Use a for loop to iterate through the dictionary.
-
+for city, info in cities.items():
+    if info["population"] > 1000000:
+        print(city)
 # 15.  How many cities are in the USA? Use .items() to get a list of tuples. Use a for loop to iterate through the list of tuples.
 # Hint 1: You will need to use the second item in the tuple. The second item is a dictionary. IE. for city, info in cities.items():
+for city, info in cities.items():
+    if info["country"] == "USA":
+        print(city)
+
+    
